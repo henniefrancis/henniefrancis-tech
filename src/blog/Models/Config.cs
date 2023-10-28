24650023eb00b9ea25909e91828e8d5723cc23b1
hennie <henniefrancis@gmail.com>
2023-10-28 18:53:54 +0200

    Career Page and Other Updates

---
+++ src/blog/Models/Config.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace blog
+{
+    public static class Config
+    {
+        public static string Logo { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["Logo"];
+        public static string LinkedIn { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["LinkedIn"];
+        public static string GitHub { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["GitHub"];
+        public static string Certifications { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["Certifications"];
+        public static string TechStack { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["TechStack"];
+        public static string Career { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["Career"];
+        public static string Capitec { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["Capitec"];
+        public static string Back { get; set; } = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Images")["Back"];
+    }
+}
---
+++ src/blog/Models/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace blog
+{
+    public static class Models
+    {
+        public static string FixName(this string input) =>
+        input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+        };
+    }
+}
