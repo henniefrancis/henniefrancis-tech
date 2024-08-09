60c10efca8b9781df877992b07a3717c986465f4
Hennie Francis <hennie@dotcoresolutions.io>
2024-08-09 23:10:45 +0200

    Resource QR's

---
+++ src/blog/Components/Pages/PublicSpeaker/AWSSummit/2024/QR-Resources.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace blog.Components.Pages
+{
+    public class AWSSummitResourcesModel : RazorBase
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await Initialize();
+        }
+    }
+}
