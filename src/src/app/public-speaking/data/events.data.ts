ce78f4a966893f4f82d61443a86fd37b55e4289a
Hennie Francis <henniefrancis@gmail.com>
2026-08-11 12:19:24 +0200

    New Events and New Certifications

--- src/src/app/public-speaking/data/events.data.ts
+++ src/src/app/public-speaking/data/events.data.ts
@@ -4,14 +4,15 @@ import d1 from './events/aws-reinvent-recap__2026.json';
 import d2 from './events/aws-summit__2024.json';
 import d3 from './events/aws-summit__2025-cl.json';
 import d4 from './events/aws-summit__2025.json';
-import d5 from './events/devops-day__2024.json';
-import d6 from './events/dotnet-conf__2023.json';
-import d7 from './events/dotnet-conf__2024.json';
-import d8 from './events/dotnet-conf__2025.json';
-import d9 from './events/eks-masterclass-cape-town__2026.json';
-import d10 from './events/google-io__2024.json';
-import d11 from './events/ispim__2025.json';
-import d12 from './events/season-of-ai__2024.json';
+import d5 from './events/aws-summit__2026.json';
+import d6 from './events/devops-day__2024.json';
+import d7 from './events/dotnet-conf__2023.json';
+import d8 from './events/dotnet-conf__2024.json';
+import d9 from './events/dotnet-conf__2025.json';
+import d10 from './events/eks-masterclass-cape-town__2026.json';
+import d11 from './events/google-io__2024.json';
+import d12 from './events/ispim__2025.json';
+import d13 from './events/season-of-ai__2024.json';
 
 export const EVENTS: Record<string, any> = {
   'aws-reinvent-recap/2025': d0,
@@ -19,12 +20,13 @@ export const EVENTS: Record<string, any> = {
   'aws-summit/2024': d2,
   'aws-summit/2025-cl': d3,
   'aws-summit/2025': d4,
-  'devops-day/2024': d5,
-  'dotnet-conf/2023': d6,
-  'dotnet-conf/2024': d7,
-  'dotnet-conf/2025': d8,
-  'eks-masterclass-cape-town/2026': d9,
-  'google-io/2024': d10,
-  'ispim/2025': d11,
-  'season-of-ai/2024': d12,
+  'aws-summit/2026': d5,
+  'devops-day/2024': d6,
+  'dotnet-conf/2023': d7,
+  'dotnet-conf/2024': d8,
+  'dotnet-conf/2025': d9,
+  'eks-masterclass-cape-town/2026': d10,
+  'google-io/2024': d11,
+  'ispim/2025': d12,
+  'season-of-ai/2024': d13,
 };
